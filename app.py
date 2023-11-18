b822ec9ead27793d93afeec933694f25600f78d3
sheena-k <141423563+sheena-k@users.noreply.github.com>
2023-11-18 05:46:08 +0000

    4

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template
+<!DOCTYPE html>
+<html>
+<body>
 
-app = Flask(__name__)
+<form action="name" method="post">
+    <label for="firstname">Enter your name: </label>
+    <input type="text" name="firstname"  required>
+    <br>
+    <input type="submit">
+</form>
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
-
-@app.route('/name')
-def hey():
-    return ('How are you')
-
-
-if __name__ == '__main__':
-    app.run()
+</body>
+</html>
