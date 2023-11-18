560ecc6a8ba3f60fbc55d601419ca8c839932b9b
sheena-k <141423563+sheena-k@users.noreply.github.com>
2023-11-18 05:55:00 +0000

    8

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_templates
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_templates('index.html')
 
 @app.route('/name')
 def hey():
