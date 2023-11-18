e30e9415f31c0761e4104f4bbabb245abb73b13e
sheena-k <141423563+sheena-k@users.noreply.github.com>
2023-11-18 05:43:52 +0000

    9

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return ('Hi')
+    return render_template('index.html')
+
+@app.route('/name')
+def hey():
+    return ('How are you')
+
 
 if __name__ == '__main__':
     app.run()
